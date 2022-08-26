5b02d7ab89533dadf469f569d39f2a4596c9066a
flash-of-genius <amhdaar@gmail.com>
2022-08-26 06:58:33 +0100

    010 Writing More Complex JSX Code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2> Expense item!!</h2>;
+  return (
+    <div>
+      <div>March 20th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.36</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
